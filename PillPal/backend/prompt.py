00ad7f0db23c5690a42fad2ad60643b1fd9038eb
Ashish Soni <ashish.soni2091@gmail.com>
2025-01-02 18:11:27 +0000

    Add prompt template and guidelines for AI content restrictions

---
+++ PillPal/backend/prompt.py
@@ -0,0 +1,63 @@
+from llama_index.core import PromptTemplate
+
+unsafe_categories = '''
+S1: Violent Crimes. 
+AI models should not create content that enables, encourages, or excuses the commission of violent crimes. Examples of violent crimes include, but are not limited to:
+                - Unlawful violence toward people (ex: terrorism, genocide, murder, hate crimes, child abuse, assault, battery, kidnapping)
+                - Unlawful violence toward animals (ex: animal abuse)
+S2: Non-Violent Crimes. 
+AI models should not create content that enables, encourages, or excuses the commission of non-violent crimes. Examples of non-violent crimes include, but are not limited to:
+                - Financial crimes (ex: fraud, scams, money laundering)
+                - Property crimes (ex: burglary, robbery, arson, vandalism)
+                - Drug crimes (ex: creating or trafficking narcotics)
+                - Weapons crimes (ex: producing unlicensed firearms)
+                - Cyber crimes (ex: hacking, spyware, malware)
+S3: Sex Crimes. 
+AI models should not create content that enables, encourages, or excuses the commission of sex crimes. Examples of sex crimes include, but are not limited to:
+                - Human trafficking
+                - Sexual assault (ex: rape)
+                - Sexual harassment (ex: groping)
+                - Lewd conduct
+S4: Child Exploitation. 
+AI models should not create content that depicts child nudity or that enables, encourages, excuses, or depicts the sexual abuse of children.
+S5: Defamation. 
+AI models should not create content about a real, living person that is verifiably false and likely to injure the person's reputation.
+S6: Privacy. 
+AI models should not create content that contains sensitive, personal information about private individuals.
+S7: Intellectual Property. 
+AI models should not create content that violates the intellectual property rights of third parties.
+S8: Indiscriminate Weapons. 
+I models should not create content that enables the creation of weapons of mass destruction. Examples of such weapons include, but are not limited to:
+                - Chemical weapons (ex: nerve gas)
+                - Biological weapons (ex: anthrax)
+                - Radiological weapons (ex: salted bombs)
+                - Nuclear weapons (ex: atomic warheads)
+                - High-yield explosive weapons (ex: cluster munitions)
+S9: Hate. 
+AI models should not create content that is hateful toward people on the basis of their protected characteristics (race, color, ethnicity, national origin, disability, religious affiliation, caste, sexual orientation, sex, gender identity and serious disease). AI models should not create content that perpetuates negative stereotypes.
+S10: Self-Harm. 
+AI models should not create content that enables, encourages, or excuses acts of intentional self-harm. Examples of such actions include, but are not limited to:
+                - Suicide
+                - Self-injury (ex: cutting)
+                - Disordered eating
+S11: Sexual Content. 
+AI models should not create depictions of nude adults or content that contains erotic descriptions or explicit depictions of sex acts.
+S12: Elections. 
+AI models should not create content that contain factually incorrect information about electoral systems and processes, including in the time, place, or manner of voting in civic elections.
+S13: Code Interpreter Abuse. 
+AI models should not create content that attempts to abuse code interpreters. Examples of code interpreter abuse include, but are not limited to:
+                - Denial of service attacks
+                - Container escapes or privilege escalation.
+'''
+
+
+
+rag_prompt = (
+    """ You are an assistant for question-answering tasks.
+Use the following context to answer the question.
+If you don't know the answer, just say that you don't know.
+Use five sentences maximum and keep the answer concise.\n
+Question: {query_str} \nContext: {context_str} \nAnswer:"""
+)
+
+llm_prompt = PromptTemplate(rag_prompt)
